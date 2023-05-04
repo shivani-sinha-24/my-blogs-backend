90f022e579657b4b2455a70d182c9ede6693e9e8
ShivaniSinha <shivanisinha24dec@gmail.com>
2023-05-04 16:05:18 +0530

    change cors origin

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://my-blogs-frontend-1f33tlyry-shivani-sinha-24.vercel.app',
   methods: 'GET,POST',
   credentials:true
   }
