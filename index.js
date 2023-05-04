99bcb1fd4519a695147879d167645940e23eecf0
ShivaniSinha <shivanisinha24dec@gmail.com>
2023-05-04 20:34:44 +0530

    added access-control-allow-credential

--- index.js
+++ index.js
@@ -13,12 +13,29 @@ const app = express();
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-  origin: '*',
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials:true
-  }
-));
+app.use(function (err, req, res, next) {
+
+  console.log(err);
+
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  //logger.error(err);
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Authorization,X-ACCESS_TOKEN,Access-Control-Allow-Headers, Origin,Accept,X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+
+
+  res.status(err.status || 500);
+  res.send('Invalid API Request ');
+});
+const corsOptions ={
+  origin:'*', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 app.use("/uploads", express.static("./uploads"));  //upload folder should in the root directory
 
 // connect mongodb
